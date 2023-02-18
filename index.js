0593fc7f39e0410c3b03f545aeaba29da7f086d2
hussain2023 <iamhussainali2@gmail.com>
2023-02-18 10:30:36 +0530

    get the data from mongodb successfully

--- index.js
+++ index.js
@@ -11,11 +11,27 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgu9cba.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const fresherJobCollection = client.db("onliKaj").collection("fresherJob");
+
+    app.get("/fresher", async (req, res) => {
+      const query = {};
+      const jobs = await fresherJobCollection.find(query).toArray();
+      res.send(jobs);
+    });
+  } catch (data) {
+    console.log(data);
+  }
+}
+run().catch((error) => console.log(error));
 
 app.get("/", async (req, res) => {
   res.send("Server is Running");
